aee0c391c701905aa1c9a2942281c0e62656a5bb
M. Marciano <49877926+marsdev26@users.noreply.github.com>
2019-12-02 22:57:37 +0200

    Update nat.py

--- nat.py
+++ nat.py
@@ -1,4 +1,21 @@
-my_list = [''.join(matrix[len(matrix) - i - 1][j + i] for i in range(i1, i2))
-           for j in range(1, len(matrix[0]))
-           for i1 in range(0, len(matrix[0]) - j)
-           for i2 in range(i1 + 1, len(matrix[0]) - j + 1)]
+
+def rotate(l, n):
+    return l[n:] + l[:n]
+
+def get_substrings(s):
+    for j in range(0, len(s)):
+        for i in range(j+1, len(s)+1):
+            yield s[j:i]
+
+def get_values(matrix):
+    transposed = [*zip(*matrix)]
+    for i in range(len(matrix[0])):
+        transposed[i] = rotate(transposed[i], -i)
+    transposed = [*zip(*transposed)]
+    for i, lst in enumerate(transposed, 1):
+        yield from get_substrings(''.join(lst[:i]))
+    for i, lst in enumerate(transposed, 1):
+        yield from get_substrings(''.join(lst[i:]))
+
+for v in get_values(matrix):
+    print(v, end=' ')
